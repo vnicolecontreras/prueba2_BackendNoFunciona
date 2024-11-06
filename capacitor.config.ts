4a232805fa50ce069fee8c9045b95dd8d21f0e7d
Veronica <veronica.contrerassaez01@gmail.com>
2024-11-06 14:57:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Veronica_Contreras_Prueba_2',
+  webDir: 'www'
+};
+
+export default config;
